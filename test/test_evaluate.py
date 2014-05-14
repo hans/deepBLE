57896999518f184c294cbb7fd60ccf8957905134
Hans Engel <engel@engel.uk.to>
2014-05-14 12:50:37 -0700

    Update test fixtures

--- test/test_evaluate.py
+++ test/test_evaluate.py
@@ -13,11 +13,12 @@ class DummyNamespace(object):
 def test_evaluate_dummy_neural():
     arguments = {
         'model': 'neural',
-        'vsm_source': 'test/fixtures/single-word/vsm-a-window5-min0-dim2',
-        'vsm_target': 'test/fixtures/single-word/vsm-b-window5-min0-dim2',
-        'data': 'test/fixtures/single-word/seed-set.tsv',
-        'model_arguments': {'hidden_layer_size': 2},
+        'vsm_source': 'test/fixtures/three-words/vsm-a-window5-min0-dim5',
+        'vsm_target': 'test/fixtures/three-words/vsm-b-window5-min0-dim5',
+        'data': 'test/fixtures/three-words/seed-set.tsv',
+        'model_arguments': {'hidden_layer_size': 5},
         'model_file': None,
+        'test_on_train': True,
     }
 
     evaluate.main(DummyNamespace(arguments))
