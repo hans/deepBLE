4997f34e20d1def15e808fc49b13d91981492168
Hans Engel <engel@engel.uk.to>
2014-05-15 09:45:24 -0700

    add test_linear

---
+++ test/test_linear.py
@@ -0,0 +1,21 @@
+from gensim.models import Word2Vec
+from nose.tools import assert_equal
+
+from model import LinearTranslationModel
+from model.runner import evaluate_model
+
+
+def load_vsm_fixture(name):
+    return Word2Vec.load('test/fixtures/{}'.format(name))
+
+
+def test_3words():
+    data = [('here', 'there'), ('now', 'then')]
+
+    model = LinearTranslationModel(
+        load_vsm_fixture('three-words/vsm-a-window5-min0-dim5'),
+        load_vsm_fixture('three-words/vsm-b-window5-min0-dim5'))
+    model.train(data)
+
+    for score in evaluate_model(model, data):
+        assert_equal(1.0, score, "Should see 100% performance on training data")
