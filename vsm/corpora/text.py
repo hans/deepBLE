5420a92acf743f1895253c44b406b21a1cd130fb
Hans Engel <engel@engel.uk.to>
2014-07-25 17:02:27 -0700

    TextCorpus: add options for preserving case, tokenization

--- vsm/corpora/text.py
+++ vsm/corpora/text.py
@@ -2,7 +2,8 @@ import logging
 
 import gensim.corpora
 from gensim.corpora.dictionary import Dictionary
-
+from gensim.corpora.textcorpus import getstream
+import gensim.utils
 
 class TextCorpus(gensim.corpora.TextCorpus):
     """A corpus class which makes some minor extensions to the Gensim
@@ -11,11 +12,15 @@ class TextCorpus(gensim.corpora.TextCorpus):
     - Support loading of pre-built dictionary
     """
 
-    def __init__(self, input=None, dictionary=None, dictionary_save_path=None):
+    def __init__(self, input=None, dictionary=None, dictionary_save_path=None,
+                 pre_tokenized=False, lowercase=False):
         super(gensim.corpora.TextCorpus, self).__init__()
 
         self.input = input
         self.metadata = False
+
+        self.pre_tokenized = pre_tokenized
+        self.lowercase = lowercase
 
         if dictionary is None:
             self.dictionary = Dictionary()
@@ -31,3 +36,19 @@ class TextCorpus(gensim.corpora.TextCorpus):
 
         if dictionary_save_path is not None:
             self.dictionary.save(dictionary_save_path)
+
+    def get_texts(self):
+        length = 0
+
+        # Input should have one document (sentence, for the word2vec case) per line
+        for line in getstream(self.input):
+            length += 1
+
+            if self.pre_tokenized:
+		if not isinstance(line, unicode):
+		    line = unicode(line, encoding='utf8', errors='strict')
+                yield line
+            else:
+                yield gensim.utils.tokenize(line, lowercase=self.lowercase)
+
+        self.length = length
